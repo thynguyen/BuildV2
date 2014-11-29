267749b1426e474169f0adb11e3ef5e108a8763c
thynguyen <thynguyen222@gmail.com>
2014-11-29 11:07:12 -0800

    build jenkins finish

---
+++ src/App/App.php
@@ -0,0 +1,116 @@
+<?php
+
+namespace App;
+
+class App
+{
+	public $con_co;
+
+    public function run()
+    {
+		$checkStyleCode______________________________________________________=9;
+		$checkBuildCode=9;
+		$checkCountCode=10;
+		if($checkStyleCode______________________________________________________==9&&$checkBuildCode==9&&$checkCountCode==100000){
+			echo 'this is xxx';		
+		}				        
+		return 'xxx';
+    }
+
+	public function xxx_aaa_aaa() {
+		$a = 'Anh nho em';
+		$a = 'Anh nho em';
+		$a = 'Anh nho em';
+		$a = 'Anh nho em';
+		$a = 'Anh nho em';
+		$a = 'Anh nho em';
+		$a = 'Anh nho em';
+		$a = 'Anh nho em';
+		$a = 'Anh nho em';
+		$a = 'Anh nho em';
+		$a = 'Anh nho em';
+		$a = 'Anh nho em';
+		$a = 'Anh nho em';
+		$a = 'Anh nho em';
+		$a = 'Anh nho em';
+		$a = 'Anh nho em';
+		$a = 'Anh nho em';
+		$a = 'Anh nho em';
+		$a = 'Anh nho em';
+		$a = 'Anh nho em';
+		$a = 'Anh nho em';
+		$a = 'Anh nho em';
+		$a = 'Anh nho em';
+		$a = 'Anh nho em';
+		$a = 'Anh nho em';
+		$a = 'Anh nho em';
+		$a = 'Anh nho em';
+		$a = 'Anh nho em';
+		$a = 'Anh nho em';
+		$a = 'Anh nho em';
+		$a = 'Anh nho em';
+		$a = 'Anh nho em';
+		$a = 'Anh nho em';
+		$a = 'Anh nho em';
+		$a = 'Anh nho em';
+		$a = 'Anh nho em';
+		$a = 'Anh nho em';
+		$a = 'Anh nho em';
+		$a = 'Anh nho em';
+		$a = 'Anh nho em';
+		$a = 'Anh nho em';
+		$a = 'Anh nho em';
+		$a = 'Anh nho em';
+		$a = 'Anh nho em';
+		$a = 'Anh nho em';
+		$a = 'Anh nho em';
+		$a = 'Anh nho em';
+		$a = 'Anh nho em';
+		$a = 'Anh nho em';
+		$a = 'Anh nho em';
+		$a = 'Anh nho em';
+		$a = 'Anh nho em';
+		$a = 'Anh nho em';
+		$a = 'Anh nho em';
+		$a = 'Anh nho em';
+		$a = 'Anh nho em';
+		$a = 'Anh nho em';
+		$a = 'Anh nho em';
+		$a = 'Anh nho em';
+		$a = 'Anh nho em';
+		$a = 'Anh nho em';
+		$a = 'Anh nho em';
+		$a = 'Anh nho em';
+		$a = 'Anh nho em';
+		$a = 'Anh nho em';
+		$a = 'Anh nho em';
+		$a = 'Anh nho em';
+		$a = 'Anh nho em';
+		$a = 'Anh nho em';
+		$a = 'Anh nho em';
+		$a = 'Anh nho em';
+		$a = 'Anh nho em';
+		$a = 'Anh nho em';
+		$a = 'Anh nho em';
+		$a = 'Anh nho em';
+		$a = 'Anh nho em';
+		$a = 'Anh nho em';
+		$a = 'Anh nho em';
+		$a = 'Anh nho em';
+		$a = 'Anh nho em';
+		$a = 'Anh nho em';
+		$a = 'Anh nho em';
+		$a = 'Anh nho em';
+		$a = 'Anh nho em';
+		$a = 'Anh nho em';
+		$a = 'Anh nho em';
+		$a = 'Anh nho em';
+		$a = 'Anh nho em';
+		$a = 'Anh nho em';
+		$a = 'Anh nho em';
+		$a = 'Anh nho em';
+		$a = 'Anh nho em';
+		$a = 'Anh nho em';
+		$a = 'Anh nho em';
+	}
+}
---
+++ src/App/Test.php~
@@ -0,0 +1,26 @@
+<?php
+/**
+ * My Application sample
+ * PHP version 5
+ *
+ * @category SampleCiWithPhpxxx
+ * @package  Test
+ * @author   Marcelo Gornstein <marcelog@gmail.com>
+ * @license  http://marcelog.github.com/ Apache License 2.0
+ * @link     http://marcelog.github.com/
+ */
+namespace Test;
+
+class Test
+{
+    /**
+     * This will run the application.
+     *
+     * @return integer
+     */
+    public function run()
+    {
+
+        return 'xxxx';
+    }
+}
